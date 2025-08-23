a93259f25a35da83d57e8b8098ed9b2e381afe9a
Yanka98 <yankavanwyk@gmail.com>
2025-08-23 15:40:40 +0200

    Update health.js

--- api/health.js
+++ api/health.js
@@ -1,1 +1,5 @@
-export default (_, res) => res.status(200).send("OK");
+// api/health.js
+export default function handler(_req, res) {
+  res.setHeader("Cache-Control", "no-store");
+  res.status(200).send("OK");
+}
